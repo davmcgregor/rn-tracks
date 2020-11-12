afca790b2a6aa276b943ded4a10a2924145e2dd5
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-12 21:05:24 +0000

    Add react-native-elements

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import {View, StyleSheet, Text} from 'react-native'
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
-const SignupScreen = () => {
-  return <Text style={{fontSize: 48}}>SignupScreen</Text>
-}
+const SignupScreen = ({ navigation }) => {
+  return (
+    <>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Input label='Email' />
+      <Spacer />
+      <Input label='Password' />
+      <Spacer>
+        <Button title='Sign Up' />
+      </Spacer>
+    </>
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 
-export default SignupScreen
+export default SignupScreen;
