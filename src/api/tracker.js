db55f46fb301b70e9ac478b596f204b990979eab
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-13 12:06:23 +0000

    Make Signup API request

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'http://13a2e4daa0fe.ngrok.io',
+});
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
   switch (action.type) {
@@ -7,8 +8,36 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return async ({ email, password }) => {
+    try {
+      const response = await trackerApi.post('/signup', { email, password });
+      console.log(response.data);
+    } catch (err) {
+      console.log(err.response.data);
+    }
+    // make api request to sign up with that emial and password
+    // if we sign up, modify state and say authenticated
+    // if signup fails, need to reflect a eror message somewhere
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // Try to signin
+    // Handle success by updating state
+    // Handle failure by showing error message
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {
+    // somehow sign out
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signin, signout, signup },
   { isSignedIn: false }
 );
