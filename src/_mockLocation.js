d9fca714a7881a159967f80d1f4a990d271219d1
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-15 17:20:09 +0000

    Create and wire up LocationContext

--- src/_mockLocation.js
+++ src/_mockLocation.js
@@ -11,8 +11,8 @@ const getLocation = (increment) => {
       accuracy: 5,
       altitudeAccuracy: 5,
       altitude: 5,
-      longitude: -122.0312186 + increment * tenMetersWithDegrees,
-      latitude: 37.33233141 + increment * tenMetersWithDegrees,
+      longitude: -0.10161 + increment * tenMetersWithDegrees,
+      latitude: 51.52925 + increment * tenMetersWithDegrees,
     },
   };
 };
